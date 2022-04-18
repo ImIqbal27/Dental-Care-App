681062d6a7ec2885f3b0a1637c1513a20dafa645
unknown <iamhasan9501@gmail.com>
2022-04-18 21:11:52 +0600

    firebase Authentication done

--- src/Pages/LoginRegPage/Register/Register.js
+++ src/Pages/LoginRegPage/Register/Register.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+    if (user) {
+        navigate('/home');
+
+    }
+
+
     const handleRegister = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password);
     }
     return (
-        <div>
 
-            <div className='container  w-50 mx-auto mb-5 pb-5'>
-                <h2 className='text-center mt-2'>Please Register</h2>
-                <Form onSubmit={handleRegister} >
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label> Your Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Name" required />
 
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" required />
+        <div className='container  w-50 mx-auto mb-5 pb-5'>
+            <h2 className='text-center mt-2'>Please Register</h2>
+            <Form onSubmit={handleRegister} >
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label> Your Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter Name" required />
 
-                    </Form.Group>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" required />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-                <p>Already have an account? <Link to='/login' className='text-danger text-decoration-none' > Login</Link></p>
-            </div>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <p>Already have an account? <Link to='/login' className='text-danger text-decoration-none' > Login</Link></p>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,21 +1,59 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    apiKey: "AIzaSyCaBPO4_wX3Cf9W-uWxR2NCRcWoaMq_8fg",
+    authDomain: "dental-care-app-3d420.firebaseapp.com",
+    projectId: "dental-care-app-3d420",
+    storageBucket: "dental-care-app-3d420.appspot.com",
+    messagingSenderId: "656762456982",
+    appId: "1:656762456982:web:745cd203756f9200dcabb7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
 export default auth;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from 'firebase/auth';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+// export default auth;
